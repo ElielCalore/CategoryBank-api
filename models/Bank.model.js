876c813ac6507f08fcb9311c9a5c12deec7b3260
Caio Garcia <caiopassador@hotmail.com>
2022-07-25 10:05:31 +0100

    fix added date to Bank Model

--- models/Bank.model.js
+++ models/Bank.model.js
@@ -3,6 +3,7 @@ const { Schema, model, Types } = require("mongoose");
 const BankSchema = new Schema({
   bankName: { type: String, required: true },
   delimiter: { type: String, required: true, trim: true },
+  date: { type: String, required: true, trim: true },
   debit: { type: String, trim: true },
   credit: { type: String, trim: true },
   amount: { type: String, trim: true },
